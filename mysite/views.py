b99c40eface708487ceeb72b2f3497bc2c2251bb
CJCU-CC <108b32710@mailst.cjcu.edu.tw>
2021-04-06 10:53:32 +0800

    建立本堂課範例網站的資料模型

--- mysite/views.py
+++ mysite/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mysite import models
+
 
 # Create your views here.
 def index(request):
-    try:
-        urid = request.GET['user_id']
-        urpass = request.GET['user_pass']
-    except:
-        urid = None
-
-    if urid != None and urpass == '12345':
-        verified = True
-    else:
-        verified = False
+    posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
+    moods = models.Mood.objects.all()
     return render(request, 'index.html', locals())
